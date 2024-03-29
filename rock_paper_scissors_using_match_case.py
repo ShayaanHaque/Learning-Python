ceebf0ae8923a91404bd3cdcc94139d4786e6b87
Shayaan Haque <mohdshayaanhaque11@gmail.com>
2024-03-29 11:20:39 +0000

    fix try

--- rock_paper_scissors_using_match_case.py
+++ rock_paper_scissors_using_match_case.py
@@ -10,34 +10,31 @@ for i in range(0,rounds):
     #Prints users input and the computers random choice
     print(f"You choose {user} and I choose {comp}")
     def play():
-        try:
-            match user:
-                case "rock":
-                    if comp == "paper" :
-                        print("One point for me ")
-                    if comp == "scissors" :
-                        print("One point for you")
-                    if comp == "rock":
-                        print("Tie")   
-                case "scissor":
-                    if comp == "paper" :
-                        print("One point for you ")
-                    if comp == "rock" :
-                        print("One point for me ")
-                    if comp == "scissor":
-                        print("Tie")
-                case "paper":
-                    if comp == "rock" :
-                        print("One point for you ")
-                    if comp == "scissors" :
-                        print("One point for me ")
-                    if comp == "paper":
-                        print("Tie")
-                # case _:
-                #     print("Maybe you should enter the correct the spelling :)")
-        except ValueError:
-            print("Mabye check the spelling :)")
-        play()
+        match user:
+            case "rock":
+                if comp == "paper" :
+                    print("One point for me ")
+                if comp == "scissors" :
+                    print("One point for you")
+                if comp == "rock":
+                    print("Tie")   
+            case "scissor":
+                if comp == "paper" :
+                    print("One point for you ")
+                if comp == "rock" :
+                    print("One point for me ")
+                if comp == "scissor":
+                    print("Tie")
+            case "paper":
+                if comp == "rock" :
+                    print("One point for you ")
+                if comp == "scissors" :
+                    print("One point for me ")
+                if comp == "paper":
+                    print("Tie")
+            case _:
+                print("Maybe you should enter the correct the spelling :)")
+    play()
 
 
-        # correct the spelling if the word is similar - (rck - rock)
+    # correct the spelling if the word is similar - (rck - rock)
