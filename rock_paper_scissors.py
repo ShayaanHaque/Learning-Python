c4d3c89f27deeabe869da0c726d3c06b5e1c1383
Shayaan Haque <mohdshayaanhaque11@gmail.com>
2024-04-02 10:23:17 +0100

    Improved version of rock paper scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,7 +1,13 @@
+
 #importing random
 import random
 #asking user for number of rounds
-rounds = int(input("How many rounds do you want "))
+while True:
+    try:
+        rounds = int(input("How many rounds do you want "))
+        break
+    except ValueError:
+        print("Please enter a number")
 for i in range(0,rounds):
     #Asks for users input
     user = input("Choose rock, paper or scissors ").strip()#Removes whitespaces
@@ -9,20 +15,34 @@ for i in range(0,rounds):
     comp  = random.choice(["rock","paper","scissors"])
     #Prints users input and the computers random choice
     print(f"You choose {user} and I choose {comp}")
+    
+    # Defines a score value for the user
+    user_score = 0
+    # Defines a score value for the computer
+    comp_score = 0
+    if user == "rock" and comp == "paper":
+        print("One point for me")
+        comp_score += 1
+    elif user == "paper" and comp == "rock":
+        print("One point for you")
+        user_score += 1
+    elif user == "rock" and comp == "scissors":
+        print("One point for you")
+        user_score += 1
+    elif user == "scissors" and comp == "rock":
+        print("One point for me")
+        comp_score += 1
+    elif user == "scissors" and comp == "paper":
+        print("One point for you")
+        user_score += 1
+    elif user == "paper" and comp == "scissors":
+        print("One point for me")
+        comp_score += 1
+    else:
+        print("Tie")
+if user_score > comp_score:
+    print("You won")
+elif user_score < comp_score:
+    print("You lost")
 
-    def play(player1,player2):
-        if player1 == "rock" and player2 == "paper":
-            print("One point for me")
-        elif player1 == "paper" and player2 == "rock":
-            print("One point for you")
-        elif player1 == "rock" and player2 == "scissors":
-            print("One point for you")
-        elif player1 == "scissors" and player2 == "rock":
-            print("One point for me")
-        elif player1 == "scissors" and player2 == "paper":
-            print("One point for you")
-        elif player1 == "paper" and player2 == "scissors":
-            print("One point for me")
-        else:
-            print("Tie")
-    play(user,comp)
+    
--- rock_paper_scissors_using_match_case.py
+++ rock_paper_scissors_using_match_case.py
@@ -1,40 +1,58 @@
 #importing random
 import random
 #asking user for number of rounds
-rounds = int(input("How many rounds do you want "))
+while True:
+    try:
+        rounds = int(input("How many rounds do you want "))
+        break
+    except ValueError:
+        print("Please enter a number")
 for i in range(0,rounds):
     #Asks for users input
-    user = input("Choose rock, paper or scissors ").strip()#Removes whitespaces
+    user = input("Choose rock, paper or scissors ").strip()#Removes whitespace
     # The computer chooses rock,paper,scissors randomly
     comp  = random.choice(["rock","paper","scissors"])
     #Prints users input and the computers random choice
     print(f"You choose {user} and I choose {comp}")
-    def play():
-        match user:
-            case "rock":
-                if comp == "paper" :
-                    print("One point for me ")
-                if comp == "scissors" :
-                    print("One point for you")
-                if comp == "rock":
-                    print("Tie")   
-            case "scissor":
-                if comp == "paper" :
-                    print("One point for you ")
-                if comp == "rock" :
-                    print("One point for me ")
-                if comp == "scissor":
-                    print("Tie")
-            case "paper":
-                if comp == "rock" :
-                    print("One point for you ")
-                if comp == "scissors" :
-                    print("One point for me ")
-                if comp == "paper":
-                    print("Tie")
-            case _:
-                print("Maybe you should enter the correct the spelling :)")
-    play()
+    user_score = 0
+    comp_score = 0
+    match user:
+        case "rock":
+            if comp == "paper" :
+                print("One point for me ")
+                comp_score += 1
+            if comp == "scissors" :
+                print("One point for you")
+                user_score += 1
+            if comp == "rock":
+                print("Tie")   
+        case "scissors":
+            if comp == "paper" :
+                print("One point for you ")
+                user_score += 1
+            if comp == "rock" :
+                print("One point for me ")
+                comp_score += 1
+            if comp == "scissors":
+                print("Tie")
+        case "paper":
+            if comp == "rock" :
+                print("One point for you ")
+                user_score += 1
+            if comp == "scissors" :
+                print("One point for me ")
+                comp_score += 1
+            if comp == "paper":
+                print("Tie")
+        case _:
+            print("Maybe you should enter the correct the spelling :)")
+            i -= 1
+if user_score > comp_score:
+    print("You won")
+elif user_score < comp_score:
+    print("You lost")
+
+        
 
 
     
